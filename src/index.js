552ff3a2a3fc82f7f4176d86f4f7c4f5026c527d
Piskunou Yahor <egor.piskunou@gmail.com>
2018-09-17 21:13:52 +0300

    done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
 module.exports = function longestConsecutiveLength(array) {
-  // your solution here
+  const set = new Set(array);
+  let longest = 0;
+
+  if(!array || array.length == 0) {
+    return longest;
+  }
+  
+  set.forEach(num => {
+    if(!set.has(num-1)) {
+      let currentNum = num;
+      let currentStreak = 1;
+      
+      while(set.has(currentNum+1)) {
+        currentNum += 1;
+        currentStreak += 1;
+      }
+      
+      longest = Math.max(longest, currentStreak);
+    }
+  });
+  
+  return longest;
 }
+
+// https://leetcode.com/articles/longest-consecutive-sequence/
